6d2daeb51ef2b5371363c3c8653328cf6ab7ddef
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-11-29 23:16:33 +0530

    Create 7. Rotate a LL.cpp

---
+++ Day 6/7. Rotate a LL.cpp
@@ -0,0 +1,91 @@
+// make the LL cyclic and then make the node head at which rotation has to start, and the node previous to it, point it to NULL.
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *rotate(struct Node *head, int k);
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        head = rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// This function should rotate list counter-clockwise
+// by k and return new head (if changed)
+
+Node* rotate(Node* head, int k)
+{
+    // Your code here
+    Node *d=head;
+    while(d->next!=NULL)
+    {
+        d=d->next;
+    }
+    d->next=head;
+    for(int i=0;i<k-1;i++)
+    {
+        head=head->next;
+    }
+    d=head;
+    head=head->next;
+    d->next=NULL;
+    return head;
+}
