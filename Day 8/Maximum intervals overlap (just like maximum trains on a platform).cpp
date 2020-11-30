649349e1c4efe5b1476ada5ee7c139200c44af7a
Nyctophiliac1918 <64465581+Nyctophiliac1918@users.noreply.github.com>
2020-11-30 10:03:12 +0530

    Create Maximum intervals overlap (just like maximum trains on a platform).cpp

---
+++ Day 8/Maximum intervals overlap (just like maximum trains on a platform).cpp
@@ -0,0 +1,47 @@
+// https://www.geeksforgeeks.org/find-the-point-where-maximum-intervals-overlap/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<int>s(n),e(n);
+	    int maxs=0,maxe=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>s[i];
+	        maxs=max(maxs,s[i]);
+	    }
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>e[i];
+	        maxe=max(maxe,e[i]);
+	    }
+	    int maxt=max(maxs,maxe);
+	    int x[maxt+5];
+	    memset(x,0,sizeof(x));
+	    for(int i=0;i<n;i++)
+	    {
+	        x[s[i]]++;
+	        x[e[i]+1]--;
+	    }
+	    int p=0,c=0,maxy=0;
+	    for(int i=0;i<maxt+2;i++)
+	    {
+	        c+=x[i];
+	        if(c>maxy)
+	        {
+	            maxy=c;
+	            p=i;
+	        }
+	    }
+	    cout<<maxy<<" "<<p<<endl;
+	}
+	return 0;
+}
